554cc3980369672a829f0b97caa2d4ba5808a88d
Simone Farina <simonefarina2004@gmail.com>
2023-09-27 18:19:55 +0200

    cloned from old java 2

---
+++ Casting #1/exercise.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      char char1 = 'a';
+      char char2 = 'b';
+      String charsSum = "" + char1 + char2;
+
+      System.out.println("Sum of chars : " + charsSum);
+    }
+}
