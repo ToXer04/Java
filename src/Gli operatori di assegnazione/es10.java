088c9c24e221bf6464aa415a09fcf5e0341a6c4f
Simone Farina <simonefarina2004@gmail.com>
2023-09-27 23:09:26 +0200

    Added assignment operators exercise 2

--- src/Gli operatori di assegnazione/es10.java
+++ src/Gli operatori di assegnazione/es10.java
@@ -1,16 +1,12 @@
-public class es9 {
+public class es10 {
     public static void main(String[] args) {
         double var1 = 1.2;
         double var2 = 99;
-        System.out.println("The value of the first variable is : " + var1);
-        System.out.println("The value of the second variable is : " + var2);
         System.out.println("The multiplication of the two increased variables is : " + moltInc(var1, var2));
     }
     public static double moltInc(double var1, double var2) {
         var1 += 3.8;
         var2 ++;
-        System.out.println("The value of the first variable increased is : " + var1);
-        System.out.println("The value of the second variable increased is : " + var2);
         double result = var1*var2;
         return result;
     }
