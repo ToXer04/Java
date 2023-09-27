0f65305a2658b5ce0d6835c8fda7889de83b6aaa
Simone Farina <simonefarina2004@gmail.com>
2023-09-27 23:24:05 +0200

    Fixed the second comparison operators exercise

--- src/Gli operatori di comparazione #2/es12.java
+++ src/Gli operatori di comparazione #2/es12.java
@@ -2,6 +2,8 @@ public class es12 {
     public static void main(String[] args) {
         char char1 = 'c';
         char char2 = 'c';
+        System.out.println("Char1 = " + char1);
+        System.out.println("Char2 = " + char2);
         System.out.println("Are char1 and char2 equal? " + charComp(char1, char2));
     }
     public static boolean charComp(char char1, char char2) {
