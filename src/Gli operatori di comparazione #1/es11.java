b62c699bf40124cfa02167017bc4414a64b4c16c
Simone Farina <simonefarina2004@gmail.com>
2023-09-27 23:18:28 +0200

    Added the first comparison operators exercise

---
+++ src/Gli operatori di comparazione #1/es11.java
@@ -0,0 +1,11 @@
+public class es11 {
+    public static void main(String[] args) {
+        int num1 = 13;
+        int num2 = 11;
+        System.out.println("Are num1 and num2 equal? " + comp(num1, num2));
+    }
+    public static boolean comp(int num1, int num2) {
+        boolean result = num1 == num2;
+        return result;
+    }
+}
