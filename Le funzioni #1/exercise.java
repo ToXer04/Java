8aa475c2a5eab5b119bcb1fdb89c04b2251a7e4c
Simone Farina <simonefarina2004@gmail.com>
2023-09-27 22:01:51 +0200

    Added func exercises

---
+++ Le funzioni #1/exercise.java
@@ -0,0 +1,11 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int num1 = 3;
+        int num2 = 7;
+        System.out.println("The sum is : " + sum(num1, num2));
+    }
+    public static int sum (int num1, int num2) {
+        int sumOf = num1 + num2;
+        return sumOf;
+    }
+}
---
+++ Le funzioni #2/exercise.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String args[]) {
+        String str = "This is test";
+        System.out.println("The length of the string is : " + calcLength(str));
+    }
+    public static int calcLength (String str) {
+        int lengthOf = str.length();
+        return lengthOf;
+    }
+}
