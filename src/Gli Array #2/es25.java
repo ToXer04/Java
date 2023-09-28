cdc7d14a77f3c806223de9a4015bd18eb651582b
Simone Farina <simonefarina2004@gmail.com>
2023-09-29 00:05:51 +0200

    Added the second array exercise

---
+++ src/Gli Array #2/es25.java
@@ -0,0 +1,19 @@
+public class es25 {
+    public static void main(String[] args) {
+        arraySearch();
+    }
+    public static void arraySearch() {
+        char[] arr = {'a', 'b', 'c', 'a', 'x', 'a'};
+        boolean areThereA = false;
+        for(char charX: arr) {
+            if(charX=='a') {
+                System.out.println(charX);
+                areThereA = true;
+            }
+        }
+        if(!areThereA) {
+            System.out.println(0);
+        }
+
+    }
+}
