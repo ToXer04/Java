bcdaa8af19cdcfc18817754ae347234baa3ae1ae
Simone Farina <simonefarina2004@gmail.com>
2023-09-28 23:46:29 +0200

    Added the second advanced loop exercise

---
+++ src/I cicli avanzati - break, continue, nested loops #2/es23.java
@@ -0,0 +1,15 @@
+public class es23 {
+    public static void main(String[] args) {
+        int val = 10;
+        invCount(val);
+    }
+    public static void invCount(int val) {
+        for(int x = 0;x<=val;x++) {
+            if(x==5) {
+                continue;
+            }
+            System.out.println(x);
+        }
+
+    }
+}
