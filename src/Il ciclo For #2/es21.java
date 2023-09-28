b21e744d00eb7b89d0fc99a25f382f364b852856
Simone Farina <simonefarina2004@gmail.com>
2023-09-28 23:36:36 +0200

    Added the second for loop exercise

--- src/Il ciclo For #2/es21.java
+++ src/Il ciclo For #2/es21.java
@@ -1,15 +1,13 @@
 public class es21 {
     public static void main(String[] args) {
-        int val = 5;
-        int x = 1;
-        System.out.println(sumOfValues(val, x));
+        int num = 10;
+        int maxRes = 15;
+        invCount(num, maxRes);
     }
-    public static int sumOfValues(int val, int x) {
-        int sum = 0;
-        while(x<val) {
-            sum += x;
-            x++;
+    public static void invCount(int num, int maxRes) {
+        for(int x = maxRes;x>0;x--) {
+            System.out.println(num);
+            num--;
         }
-        return sum;
     }
 }
