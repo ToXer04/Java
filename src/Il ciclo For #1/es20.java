43cccda57aaa3b71386e12b82385b99cc5ec6732
Simone Farina <simonefarina2004@gmail.com>
2023-09-28 23:32:06 +0200

    Added the first for loop exercise

---
+++ src/Il ciclo For #1/es20.java
@@ -0,0 +1,11 @@
+public class es20 {
+    public static void main(String[] args) {
+        int val = 0;
+        arithmeticMulti(val);
+    }
+    public static void arithmeticMulti(int val) {
+        for(int x = 1;x<=10;x++) {
+            System.out.println(val*x);
+        }
+    }
+}
