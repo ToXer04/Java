c6e7f0f05d7ae74395d08431c17634b692163148
Simone Farina <simonefarina2004@gmail.com>
2023-09-29 01:37:26 +0200

    Added the second multidimensional array exercise

---
+++ src/Gli Array multidimensionali #2/es28.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class es28 {
+    public static void main(String[] args) {
+        arrayMultSum();
+    }
+    public static void arrayMultSum() {
+        int[][] arr = {{1, 2, 3}, {4, 5, 6}};
+        int[][] newArr = new int[arr[0].length][arr.length];
+        for(int i = 0; i<arr.length; i++) {
+            for(int j = 0; j<arr[i].length; j++) {
+                newArr[j][i] = arr[i][j];
+            }
+        }
+        System.out.println(Arrays.deepToString(newArr));
+    }
+}
